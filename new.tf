2e0aa305a0fee002edb70deb66f8968bd9acbb9b
ortalgabay <108272041+ortalgabay@users.noreply.github.com>
2023-01-05 11:28:40 +0200

    Update new.tf

--- new.tf
+++ new.tf
@@ -15,8 +15,8 @@ sudo systemctl enable apache2
 export AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMAAA
 export AWS_SECRET_ACCESS_KEY=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
 export AWS_DEFAULT_REGION=us-west-2
-export BLA=zehavit1234
-export BLA=zehavit4444
+export WEC=zehavit1234
+export AAA=zehavit4444
 export BLA=ortal1334
 echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
 EOF
